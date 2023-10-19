5c74aec7e6e30a069e7f4dcf0503b461948ae43a
tallentedhub <147305874+tallentedhub@users.noreply.github.com>
2023-10-19 19:47:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,42 @@
-const signInForm = document.getElementById('sign-in-form');
+// Automatically sign in as guest ID.
+const guestId = Math.random().toString(36).substring(7);
+
+// Connect to the database.
+const db = new PDO('mysql:host=localhost;dbname=whatsapp_clone', 'root', '');
+
+// Get the current group ID.
+const groupId = document.getElementById('group-id').value;
+
+// Get the chat box element.
 const chatBox = document.getElementById('chat-box');
+
+// Get the message input element.
 const messageInput = document.getElementById('message-input');
+
+// Get the send button element.
 const sendButton = document.getElementById('send-button');
 
-// Sign in as a guest.
-signInForm.addEventListener('submit', function(event) {
-  event.preventDefault();
+// Get the join group button element.
+const joinGroupButton = document.getElementById('join-group-button');
 
-  const guestId = document.getElementById('guest-id').value;
+// Load the chat history for the current group.
+loadChatHistory(groupId);
 
-  // Join the chat room.
-  // ...
-
-  // Hide the sign in form and show the chat box.
-  signInForm.style.display = 'none';
-  chatBox.style.display = 'block';
-});
-
-// Send a message.
+// Add an event listener to the send button.
 sendButton.addEventListener('click', function() {
-  const message = messageInput.value;
+  // Get the message text.
+  const messageText = messageInput.value;
 
-  // Send the message to the chat room.
-  // ...
+  // Send the message to the database.
+  db.query('INSERT INTO messages (guest_id, group_id, message) VALUES (?, ?, ?)', [guestId, groupId, messageText]);
 
   // Clear the message input.
   messageInput.value = '';
+
+  // Update the chat history.
+  loadChatHistory(groupId);
 });
+
+// Add an event listener to the join group button.
+joinGroupButton.addEventListener('click', function() {
+  // Get the group ID to join
