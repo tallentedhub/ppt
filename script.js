80d59d86565f422c533e7643d3a5aa38b69bd259
tallentedhub <147305874+tallentedhub@users.noreply.github.com>
2023-10-19 10:25:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
+const signInForm = document.getElementById('sign-in-form');
 const chatBox = document.getElementById('chat-box');
 const messageInput = document.getElementById('message-input');
 const sendButton = document.getElementById('send-button');
 
+// Sign in as a guest.
+signInForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const guestId = document.getElementById('guest-id').value;
+
+  // Join the chat room.
+  // ...
+
+  // Hide the sign in form and show the chat box.
+  signInForm.style.display = 'none';
+  chatBox.style.display = 'block';
+});
+
+// Send a message.
 sendButton.addEventListener('click', function() {
   const message = messageInput.value;
 
-  // Add the message to the chat box.
-  chatBox.innerHTML += `<li>${message}</li>`;
+  // Send the message to the chat room.
+  // ...
 
   // Clear the message input.
   messageInput.value = '';
